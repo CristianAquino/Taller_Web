f3195f9250693ef7bcb980c2981a9a8ac8f1b2f4
cristianaquino <ycristian10@gmail.com>
2022-09-08 15:28:10 -0500

    Tarea4

--- Tarea4/consult.php
+++ Tarea4/consult.php
@@ -8,50 +8,50 @@
             <input type="submit" name="Buscar" value="Buscar">
         </form>
         <div class="doc">
-        <?php
-            if (isset($_GET['Buscar'])){
-                $v2 = $_GET['Nombre'];    
+            <?php
+            if (isset($_GET['Buscar'])) {
+                $v2 = $_GET['Nombre'];
                 $sql = "select * from Perro where Nombre like '$v2%'";
                 $result = mysqli_query($conn, $sql);
-                while($row = mysqli_fetch_array($result)){?>
-                <div class="mostrar">
-                    <div class="imgBx">
-                        <?php if($row['Foto']) {?>
-                            <img src=<?php echo $row['Foto'] ?> alt="">
-                        <?php }else{?>
-                            <img src="img/ejemplo.jpg" alt="">
-                        <?php }?>
+                while ($row = mysqli_fetch_array($result)) { ?>
+            <div class="mostrar">
+                <div class="imgBx">
+                    <?php if ($row['Foto']) { ?>
+                    <img src=<?php echo $row['Foto'] ?> alt="">
+                    <?php } else { ?>
+                    <img src="https://i.postimg.cc/TPSjLdnb/ejemplo.webp" alt="">
+                    <?php } ?>
+                </div>
+                <div class="salida">
+                    <div class="data">
+                        <h3>DNI</h3>
+                        <span><?php echo $row['DNI'] ?></span>
                     </div>
-                    <div class="salida">
-                        <div class="data">
-                            <h3>DNI</h3>
-                            <span><?php echo $row['DNI'] ?></span>
-                        </div>
-                        <div class="data">
-                            <h3>Nombre</h3>
-                            <span><?php echo $row['Nombre'] ?></span>
-                        </div>
-                        <div class="data">
-                            <h3>Nacimiento</h3>
-                            <span><?php echo $row['FechaNacimiento'] ?></span>
-                        </div>
-                        <div class="data">
-                            <h3>Raza</h3>
-                            <span><?php echo $row['Raza'] ?></span>
-                        </div>
-                        <div class="data">
-                            <h3>Genero</h3>
-                            <?php if($row['Genero'] == '0') {?>
-                                <span><?= 'Hembra' ?></span>
-                            <?php }else{?>
-                                <span><?= 'Macho' ?></span>
-                            <?php }?>
-                        </div>
+                    <div class="data">
+                        <h3>Nombre</h3>
+                        <span><?php echo $row['Nombre'] ?></span>
                     </div>
-                    <?php }?>
+                    <div class="data">
+                        <h3>Nacimiento</h3>
+                        <span><?php echo $row['FechaNacimiento'] ?></span>
+                    </div>
+                    <div class="data">
+                        <h3>Raza</h3>
+                        <span><?php echo $row['Raza'] ?></span>
+                    </div>
+                    <div class="data">
+                        <h3>Genero</h3>
+                        <?php if ($row['Genero'] == '0') { ?>
+                        <span><?= 'Hembra' ?></span>
+                        <?php } else { ?>
+                        <span><?= 'Macho' ?></span>
+                        <?php } ?>
+                    </div>
+                </div>
+                <?php } ?>
                 <div>
-            <?php }?>
+                    <?php } ?>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<?php include('includes/footer.php') ?>
+        <?php include('includes/footer.php') ?>
